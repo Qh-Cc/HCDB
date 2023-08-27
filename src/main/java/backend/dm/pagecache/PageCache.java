ec672002bc5c36ae8bb133e94c5ae4116953c30d
hqc <3457919526@qq.com>
2023-08-27 16:24:36 +0800

    页面缓存接口

--- src/main/java/backend/dm/pagecache/PageCache.java
+++ src/main/java/backend/dm/pagecache/PageCache.java
@@ -1,11 +1,18 @@
 package backend.dm.pagecache;
 
 import backend.dm.page.Page;
+import backend.utils.Panic;
+import common.Error;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 
 /**
  * @PROJECT_NAME: HCDB
  * @DESCRIPTION:
- * @Author hqc
+ * @Author Ccc
  * @DATE: 2023/8/4 11:41
  */
 public interface PageCache {
@@ -21,4 +28,47 @@ public interface PageCache {
     int getPageNumber();
     void flushPage(Page pg);
 
+    public static PageCacheImpl create(String path, long memory) {
+        File f = new File(path+PageCacheImpl.DB_SUFFIX);
+        try {
+            if(!f.createNewFile()) {
+                Panic.panic(Error.FileExistsException);
+            }
+        } catch (Exception e) {
+            Panic.panic(e);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Error.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+        return new PageCacheImpl(raf, fc, (int)memory/PAGE_SIZE);
+    }
+
+    public static PageCacheImpl open(String path, long memory) {
+        File f = new File(path+PageCacheImpl.DB_SUFFIX);
+        if(!f.exists()) {
+            Panic.panic(Error.FileNotExistsException);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Error.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+        return new PageCacheImpl(raf, fc, (int)memory/PAGE_SIZE);
+    }
 }
